9de5351594881d00d3b0b632940e4145ccbe242c
Aaron Board <Aaron Board>
2017-08-26 10:22:57 -0400

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {HttpModule} from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
+import {MdSlideToggleModule} from '@angular/material';
+import {MdTabsModule} from '@angular/material';
+import {MdSliderModule} from '@angular/material';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdCardModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdSlideToggleModule,
+    MdTabsModule,
+    MdSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
